2a4dc602fe49a8419efa481ed21309e844561c3f
alvi103-png <alvi103@gmail.com>
2026-07-28 13:40:24 +0200

    chore: remove broken App.css import and empty stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/layout/Header/Header";
 import Footer from "./components/layout/Footer/Footer";
 import BottomNav from "./components/layout/BottomNav/BottomNav.jsx";
-import "./App.css";
-
 
 function App() {
     return (
