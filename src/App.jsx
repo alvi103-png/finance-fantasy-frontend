a5399e63b10fde16eda95c95ece0895cbd0e4ea0
alvi103-png <alvi103@gmail.com>
2026-07-28 13:32:54 +0200

    fix: imports missing so the responsive was failing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/layout/Header/Header";
 import Footer from "./components/layout/Footer/Footer";
 import BottomNav from "./components/layout/BottomNav/BottomNav.jsx";
+import "./App.css";
 
 
 function App() {
